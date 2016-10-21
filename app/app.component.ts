017d95a43cd7283f29b6be4945976b60cb60c05f
Mark Lawson <mlawson3691@gmail.com>
2016-10-21 08:32:42 -0700

    create a food-list component with sample Food data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { Food }      from './food.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class='container'>
     <h1>Meal Tracker</h1>
+    <food-list
+      [childFoodList]="masterFoodList"
+    ></food-list>
   </div>
   `
 })
 
 export class AppComponent {
-
+  public masterFoodList: Food[] = [
+    new Food('Apple', 'A granny smith apple.', 100),
+    new Food('Potato Chips', 'I ate the whole bag! :(', 1000),
+    new Food('Steak', 'A special dinner out', 700),
+    new Food('Baked Potato', 'To go with the steak', 160),
+    new Food('Brussel Sprouts', 'Healthy side dish!', 40)
+  ];
 }
---
+++ app/food-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Food }             from './food.model';
+
+@Component({
+  selector: 'food-list',
+  template: `
+  <p>Test</p>
+  `
+})
+
+export class FoodListComponent {
+
+}
