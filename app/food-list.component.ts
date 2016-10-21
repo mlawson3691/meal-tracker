ab5d60682da048872029c817a2718ffa9c3241d8
Mark Lawson <mlawson3691@gmail.com>
2016-10-21 08:38:52 -0700

    display all foods on the page

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -4,10 +4,15 @@ import { Food }             from './food.model';
 @Component({
   selector: 'food-list',
   template: `
-  <p>Test</p>
+  <h3>Total Calories Today: ____</h3>
+  <div *ngFor="let food of childFoodList" class='well'>
+    <h4>{{ food.name }}</h4>
+    <p><b>Details:</b> {{ food.details }}</p>
+    <p><b>Calories:</b> {{ food.calories}}</p>
+  </div>
   `
 })
 
 export class FoodListComponent {
-
+  @Input() childFoodList: Food[];
 }
